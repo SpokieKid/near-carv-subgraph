5c9e365f8b0f8013f7a30738361e872eb0c5012a
SpokieKid <assassins010@outlook.com>
2022-07-06 04:42:46 +0000

    update subgraph

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,34 +1,77 @@
-import { near, BigInt } from "@graphprotocol/graph-ts"
-import { ExampleEntity } from "../generated/schema"
+import { near, log, json, JSONValueKind } from "@graphprotocol/graph-ts"
+import { UserNftStat } from "../generated/schema"
 
-export function handleReceipt(
-  receiptWithOutcome: near.ReceiptWithOutcome
-): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(receiptWithOutcome.receipt.id.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(receiptWithOutcome.receipt.id.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+export function handleReceipt(receipt: near.ReceiptWithOutcome): void {
+  const actions = receipt.receipt.actions;
+  for (let i = 0; i < actions.length; i++) {
+    handleAction(actions[i], receipt.receipt, receipt.block.header, receipt.outcome);
   }
+}
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+function handleAction(
+  action: near.ActionValue,
+  receipt: near.ActionReceipt,
+  blockHeader: near.BlockHeader,
+  outcome: near.ExecutionOutcome
+): void {
+  if (action.kind != near.ActionKind.FUNCTION_CALL) {
+    log.info("Early return: {}", ["Not a function call"])
+    return;
+  }
+  const functionCall = action.toFunctionCall();
 
-  // Entity fields can be set based on receipt information
-  entity.block = receiptWithOutcome.block.header.hash
+  if (functionCall.methodName == "nft_transfer") {
+    let user = UserNftStat.load(receipt.signerId);
+    if (user == null) {
+      user = new UserNftStat(receipt.signerId);
+      user.nftsIn = receipt.receiverId;
+      user.save()
+    }
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
+    // Test ends here
 
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
+
+    // Test if implementation-two's model will work on this contract
+
+    if (outcome.logs[0]!=null) {
+
+      let parsed = json.fromString(outcome.logs[0]);
+      if (parsed.kind == JSONValueKind.OBJECT) {
+
+        let entry = parsed.toObject();
+
+        // EVENT_JSON
+
+        let eventJSON = entry.entries[0].value.toObject()
+
+        for(let i = 0; i < eventJSON.entries.length; i++) {
+          let key = eventJSON.entries[i].key.toString();
+          switch (true) {
+            case key == "event":
+              user.userAddress = eventJSON.entries[i].value.toString();
+              break;
+          }
+        }
+
+        // DATA
+
+        // let data = entry.entries[0].value.toObject()
+        // for (let i = 0; i < data.entries.length; i++) {
+        //   let key = data.entries[i].key.toString();
+        //   switch (true) {
+        //     case key == "owner_id":
+        //       user.ftsTransfered = data.entries[i].value.toString();
+        //       break
+        //     // case key == "token_ids":
+        //     //   user.nftsIn = data.entries[i].value.toString();
+        //     //   break
+        //   }
+        // }
+        user.save()
+
+  } else {
+    log.info("Event and Data Not processed - FunctionCall is: {}", [functionCall.methodName]);
+  }
+  }
+  }
 }
